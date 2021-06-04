bc1416618c8af13f184e670f336534f6fec2bca2
Vishnu Paul <paulvishnu567@gmail.com>
2021-06-04 08:53:05 +0530

    adding the task left FUNCTIONALITY and improve in code by adding multiple function

--- app.js
+++ app.js
@@ -1,31 +1,74 @@
-const addBtn = document.querySelector('.fa-plus')
-const tasks = document.querySelector('.tasks')
-const input = document.querySelector('#add-task')
+const addBtn = document.querySelector('.add');
+const tasks = document.querySelector('.tasks');
+const input = document.querySelector('#add-task');
+const clearBtn = document.querySelector('.clr-btn');
 
-addBtn.addEventListener('click', function () {
-  let todoText = input.value
-  console.log(todoText)
-  if (todoText === '') {
-    alert('you must add a task')
-  } else {
-    let li = document.createElement('li')
-    li.innerHTML = `<span class="check"><input type="checkbox" name="" id="" /></span><p>
+var todos = [];
+
+addBtn.addEventListener('click', addItem);
+
+clearBtn.addEventListener('click', clearItems);
+
+function addItem(e) {
+  e.preventDefault();
+  const todoText = input.value;
+
+  if (todoText !== '') {
+    const li = document.createElement('li');
+
+    li.classList.add('list');
+    li.innerHTML = `<span class="check"><input type="checkbox" /></span><p>
       ${todoText}
-      </p><span class="remove"><i class="far fa-times-circle"></i></span>`
+      </p><span class="remove"><i class="far fa-trash-alt"></i></span>`;
 
-    tasks.insertBefore(li, tasks.childNodes[0])
-    input.value = ''
-    const remove = li.querySelector('.remove')
-    remove.addEventListener('click', function (e) {
-      console.log(this.parentNode)
-      tasks.removeChild(this.parentNode)
-    })
-    const check = li.querySelector('.check')
-    console.log(check)
-    check.addEventListener('click', function () {
-      console.log(this.parentNode)
-      let dc = this.parentNode
-      dc.classList.toggle('completed')
-    })
+    tasks.appendChild(li);
+    input.value = '';
+
+    todos.push(todoText);
+
+    clearBtn.classList.add('show-clrbtn');
+
+    const remove = li.querySelector('.remove');
+    remove.addEventListener('click', deleteItem);
+
+    const check = li.querySelector('.check');
+    check.addEventListener('click', checkItem);
+
+    count();
+  } else {
+    alert('you must add a task');
   }
-})
+}
+
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement;
+  console.log(element);
+  tasks.removeChild(element);
+  todos.pop();
+  count();
+}
+
+function checkItem(e) {
+  const element = e.currentTarget.parentElement;
+  console.log(element);
+  element.classList.toggle('completed');
+}
+
+function count() {
+  const count = document.querySelector('.num');
+  count.innerHTML = todos.length;
+}
+
+function clearItems() {
+  const items = document.querySelectorAll('.list');
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      tasks.removeChild(item);
+    });
+    clearBtn.classList.remove('show-clrbtn');
+    todos = [];
+    count();
+  } else {
+    alert('you must add a task');
+  }
+}
