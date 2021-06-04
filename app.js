b1be7090d19a48553c6c1f3dfccf7303d7085f2a
Vishnu Paul <paulvishnu567@gmail.com>
2021-06-04 12:24:42 +0530

    adding comments

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// ****** select items **********
 const addBtn = document.querySelector('.add');
 const tasks = document.querySelector('.tasks');
 const input = document.querySelector('#add-task');
 const clearBtn = document.querySelector('.clr-btn');
 
+// array to store the element and count it
 var todos = [];
 
+// ****** event listeners **********
 addBtn.addEventListener('click', addItem);
 
 clearBtn.addEventListener('click', clearItems);
 
+// add new task
 function addItem(e) {
   e.preventDefault();
   const todoText = input.value;
@@ -20,26 +24,27 @@ function addItem(e) {
     li.innerHTML = `<span class="check"><input type="checkbox" /></span><p>
       ${todoText}
       </p><span class="remove"><i class="far fa-trash-alt"></i></span>`;
-
+    // append child
     tasks.appendChild(li);
     input.value = '';
 
     todos.push(todoText);
 
     clearBtn.classList.add('show-clrbtn');
-
+    // add event listeners to remove and check buttons;
     const remove = li.querySelector('.remove');
     remove.addEventListener('click', deleteItem);
 
     const check = li.querySelector('.check');
     check.addEventListener('click', checkItem);
-
+    // count function to find the total tasks left
     count();
   } else {
     alert('you must add a task');
   }
 }
 
+// delete a task
 function deleteItem(e) {
   const element = e.currentTarget.parentElement;
   console.log(element);
@@ -48,17 +53,19 @@ function deleteItem(e) {
   count();
 }
 
+// checked a task
 function checkItem(e) {
   const element = e.currentTarget.parentElement;
   console.log(element);
   element.classList.toggle('completed');
 }
 
+// count the total task
 function count() {
   const count = document.querySelector('.num');
   count.innerHTML = todos.length;
 }
-
+// clear all tasks
 function clearItems() {
   const items = document.querySelectorAll('.list');
   if (items.length > 0) {
